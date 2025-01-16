91bd44e85823cd8366f9d2ad709380a5160c509f
CyprianZTL <156000088+CyprianZTL@users.noreply.github.com>
2025-01-16 19:15:24 +0100

    Add files via upload

---
+++ myproject/blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def create_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Post.objects.create(title=title, content=content)
+        return redirect('post_list')
+    return render(request, 'blog/create_post.html')
